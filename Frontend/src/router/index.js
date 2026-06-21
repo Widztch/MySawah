0961de9f4e241f79bfdfa91888ff063340584bc1
Widztch <widodo.etch@gmail.com>
2026-06-21 23:53:36 +0700

    Add AdminDashboard
    Penambahan Admin Dashboard view

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useProfileStore } from '@/stores/profileStore'
+import { useLoadingStore } from '@/stores/loadingStore'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
@@ -8,6 +10,8 @@ import CartView from '@/views/user/CartView.vue'
 import HistoryView from '../views/user/HistoryView.vue'
 import UserProfile from '../views/user/UserProfile.vue'
 import AdminLayout from '../components/layout/AdminLayout.vue'
+import AdminDashboard from '../views/admin/AdminDashboard.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -60,11 +64,72 @@ const router = createRouter({
       component: AdminLayout,
       meta: { requiresAdmin: true },
       children: [
+        {
+          path: 'dashboard',
+          name: 'admin-dashboard',
+          component: AdminDashboard
+        },
       ]
     },
   ]
 })
 
+
+// ==========================================
+// NAVIGATION GUARD - SEBELUM MASUK HALAMAN
+// ==========================================
+router.beforeEach(async (to, from, next) => {
+  // 1. HIDUPKAN LOADING SCREEN SAAT PINDAH HALAMAN
+  const loadingStore = useLoadingStore()
+  loadingStore.startLoading()  
+  next()
+})
+
+// ==========================================
+// SETELAH HALAMAN BERHASIL DIMUAT
+// ==========================================
+router.afterEach(() => {
+  const loadingStore = useLoadingStore()
+  
+  // Matikan loading screen. 
+  // Kita beri jeda sedikit (300ms) agar transisinya terasa lebih mulus dan tidak sekadar berkedip.
+  setTimeout(() => {
+    loadingStore.stopLoading()
+  }, 300)
+})
+
+
+// ==========================================
+// NAVIGATION GUARD 
+// ==========================================
+router.beforeEach(async (to, from, next) => {
+  // Hanya jalankan pemeriksaan jika rute tersebut dilindungi
+  if (to.meta.requiresAuth || to.meta.requiresAdmin) {
+    const profileStore = useProfileStore()
+    // 1. Jika data user belum termuat di memori (misal karena me-refresh halaman), 
+    // kita paksa fetch data dari backend terlebih dahulu sebelum halaman dibuka.
+    if (!profileStore.user) {
+      await profileStore.fetchUser()
+    }
+
+    // 2. CEK PENYUSUP ADMIN PANEL
+    if (to.meta.requiresAdmin && !profileStore.isAdmin) {
+      // Jika bukan admin, Kirim ke halaman 404 NotFound seolah-olah agar halamannya tidak ada
+      return next({ 
+        name: 'login', 
+        params: { pathMatch: to.path.substring(1).split('/') } 
+      })
+    }
+
+    // 3. CEK PENGGUNA BELUM LOGIN
+    if (to.meta.requiresAuth && !profileStore.isLoggedIn) {
+      // Tampilkan peringatan lalu arahkan ke halaman login
+      return next('/login') 
+    }
+  }
+  // Jika semua pengecekan aman, izinkan lewat
+  next()
+})
 
 
 export default router
